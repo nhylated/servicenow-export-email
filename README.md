0942abf19f6002e6491f0f6d63d8b9b7bf51c5f2
Harsh Dhulia <harshdhulia88@gmail.com>
2016-02-29 23:45:29 +1100

    Minor refactoring of code and updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # servicenow-export-email
-Adds a UI action on the sys_email table to export an email to an .EML file.
+Adds an UI action on the `sys_email` table to export an email to an .EML file.
+
+Attachments of emails are not exported to the .EML file.
+
+The UI action creates the .EML file as a ServiceNow attachment (`sys_attachment` table) and then downloads it.
+
+Emails with content-type text/html and text/plain can be exported. Multipart emails are not exported properly currently. See #1.
+
--- ui-action-script-export-to-eml.js
+++ ui-action-script-export-to-eml.js
@@ -1,23 +1,31 @@
-exportEmailToEML(current);
+var emlAttachmentSysId = exportEmailToEML(current);
+action.setRedirectURL('sys_attachment.do?sys_id=' + emlAttachmentSysId);
 
-function exportEmailToEML(/*GlideRecord (sys_email) */ gr) {
+/**
+ * Generates the string content of the .EML file for a given email and creates the EML file as an attachment and returns its sys_id.
+ * @param {GlideRecord} sys_email_gr - A sys_email GlideRecord.
+ * @return {string} sys_id of the .EML file attachment in the sys_attachment table.
+ */
+function exportEmailToEML(sys_email_gr) {
 	var emlStr = '';
-	emlStr += 'To: ' + gr.recipients + '\n';
-	emlStr += 'Subject: ' + gr.subject + '\n';
+	emlStr += 'To: ' + sys_email_gr.recipients + '\n';
+	emlStr += 'Subject: ' + sys_email_gr.subject + '\n';
 	emlStr += 'From: ' + gs.getProperty('instance_name') + '@service-now.com\n';
 	emlStr += 'Reply-To: ' + gs.getProperty('instance_name') + '@service-now.com\n';
-	emlStr += 'Content-Type: ' + gr.content_type + '\n';
-	emlStr += 'Date: ' + generateEMLDate(gr) + '\n';
-	emlStr += gr.headers + '\n';
-	emlStr += '\n\n' + gr.body;
-	var emlAttachmentSysId = (new GlideSysAttachment()).write((new GlideRecord('sys_attachment')), gr.subject + '.eml', 'text/plain', emlStr);
-	action.setRedirectURL('sys_attachment.do?sys_id=' + emlAttachmentSysId);
+	emlStr += 'Content-Type: ' + sys_email_gr.content_type + '\n';
+	emlStr += 'Date: ' + generateEMLDate(sys_email_gr) + '\n';
+	emlStr += sys_email_gr.headers + '\n';
+	emlStr += '\n\n' + sys_email_gr.body;
+	return (new GlideSysAttachment()).write((new GlideRecord('sys_attachment')), sys_email_gr.subject + '.eml', 'text/plain', emlStr);
 }
 
-
-function generateEMLDate(/*GlideRecord (sys_email) */ gr) {
-	// Construct the date string required by the EML format. (Sat, 30 Apr 2005 19:28:29 -0300)
-	var gdt = new GlideDateTime(gr.sys_created_on);
+/**
+ * Constructs the date string for the EML file for a given email's sys_created_on date. (e.g. Sat, 30 Apr 2005 19:28:29 -0300).
+ * @param {GlideRecord} sys_email_gr - A sys_email GlideRecord.
+ * @return {string} The sys_created_on date of the email in the format required by the EML file.
+ */
+function generateEMLDate(sys_email_gr) {
+	var gdt = new GlideDateTime(sys_email_gr.sys_created_on);
 	var dayOfWeek = gdt.getDayOfWeekLocalTime().toString();
 	var dayOfMonthStr = gdt.getDayOfMonthLocalTime().toString();
 	var month = gdt.getMonthLocalTime();
@@ -29,12 +37,13 @@ function generateEMLDate(/*GlideRecord (sys_email) */ gr) {
 	var tzOffsetHoursAbs = Math.abs(tzOffsetHours);
 	var tzOffsetHoursAbsStr = tzOffsetHoursAbs.toString();
 
-	var allDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-	var allMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	var emlDateStr = allDays[dayOfWeek]
+	var ALL_DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	var ALL_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+	var emlDateStr = ALL_DAYS[dayOfWeek]
 	 + ", "
 	 + ("00".slice(dayOfMonthStr.length) + dayOfMonthStr) + " "
-	 + allMonths[month - 1] + " "
+	 + ALL_MONTHS[month - 1] + " "
 	 + year + " "
 	 + localTimeStr.slice(11) + " "
 	 + tzOffsetHoursSign
